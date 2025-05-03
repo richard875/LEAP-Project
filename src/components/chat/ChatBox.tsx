95e28c9a9cc3d9f099a8ca865c7f4bdaac931c3c
Richard Lee <hello@richard-lee.com>
2025-05-04 01:10:42 +1000

    - Feature: Add ChatBox multi-line support

--- src/components/chat/ChatBox.tsx
+++ src/components/chat/ChatBox.tsx
@@ -2,27 +2,37 @@
 
 import Image from "next/image";
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { faCircleArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ChatBox = () => {
   const { systemTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
+  const [message, setMessage] = useState("");
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
 
   useEffect(() => {
     setMounted(true);
   }, []);
+
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = "auto";
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    }
+  }, [message]);
 
   if (!mounted) return null;
 
   return (
     <footer className="absolute bottom-0 left-0 w-full pl-5 pr-5">
       <div className="max-w-[800px] m-auto flex flex-col items-center justify-around">
-        <div className="w-full h-14 pl-4 pr-4 bg-neutral-50 dark:bg-neutral-700 border border-stone-300 dark:border-stone-500 rounded-4xl shadow-xs flex items-center justify-between">
-          <div className="w-full flex items-center">
+        <div className="w-full min-h-14 pl-4 pr-4 bg-neutral-50 dark:bg-neutral-700 border border-stone-300 dark:border-stone-500 rounded-4xl shadow-xs flex items-center justify-between">
+          <div className="w-full flex items-center pt-2 pb-2">
             {systemTheme === "light" ? (
               <Image
+                className="select-none"
                 src="/logo-small-light.png"
                 alt="LawConnect Logo"
                 width={24}
@@ -31,6 +41,7 @@ const ChatBox = () => {
               />
             ) : (
               <Image
+                className="select-none"
                 src="/logo-small-dark.png"
                 alt="LawConnect Logo"
                 width={24}
@@ -38,10 +49,13 @@ const ChatBox = () => {
                 priority
               />
             )}
-            <input
-              type="text"
+            <textarea
+              ref={textareaRef}
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
               placeholder="Ask a question..."
-              className="w-full focus:outline-none pl-3 pr-3"
+              rows={1}
+              className="w-full resize-none focus:outline-none pl-3 pr-3"
             />
           </div>
           <FontAwesomeIcon
