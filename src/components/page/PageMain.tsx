a142be9307472a1ac83577c9e2313e18852951e3
Richard Lee <hello@richard-lee.com>
2025-05-03 23:11:54 +1000

    - Style: Add and apply dark mode

--- src/components/page/PageMain.tsx
+++ src/components/page/PageMain.tsx
@@ -1,17 +1,42 @@
+"use client";
+
 import Image from "next/image";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const PageMain = () => {
+  const { systemTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return (
     <>
-      <Image
-        className="fixed top-6 left-5 select-none"
-        src="/logo.webp"
-        alt="LawConnect Logo"
-        width={172.2}
-        height={23.8}
-        priority
-      />
+      {systemTheme === "light" ? (
+        <Image
+          className="fixed top-6 left-5 select-none"
+          src="/logo-light.webp"
+          alt="LawConnect Logo"
+          width={172.2}
+          height={23.8}
+          priority
+        />
+      ) : (
+        <Image
+          className="fixed top-6 left-5 select-none"
+          src="/logo-dark.webp"
+          alt="LawConnect Logo"
+          width={172.2}
+          height={23.8}
+          priority
+        />
+      )}
+
       <Avatar className="fixed top-5 right-5 select-none">
         <AvatarImage src="https://github.com/richard875.png" />
         <AvatarFallback>RL</AvatarFallback>
