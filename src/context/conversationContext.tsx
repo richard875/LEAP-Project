be26a3923d4ed6344ca94c35fb61df9f5ef504df
Richard Lee <hello@richard-lee.com>
2025-05-04 23:42:51 +1000

    - Feature: Add loading and setLoading to conversationContext

--- src/context/conversationContext.tsx
+++ src/context/conversationContext.tsx
@@ -5,6 +5,8 @@ import { createContext, useState, ReactNode } from "react";
 import ConversationItem from "@/types/conversationItem";
 
 type ConversationContextType = {
+  loading: boolean;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
   conversation: ConversationItem[];
   setConversation: React.Dispatch<React.SetStateAction<ConversationItem[]>>;
 };
@@ -14,8 +16,9 @@ const ConversationContext = createContext<ConversationContextType | undefined>(
 );
 
 export const ConversationProvider = ({ children }: { children: ReactNode }) => {
+  const [loading, setLoading] = useState(false);
   const [conversation, setConversation] = useState<ConversationItem[]>([]);
-  const contextValue = { conversation, setConversation };
+  const contextValue = { conversation, setConversation, loading, setLoading };
 
   return (
     <ConversationContext.Provider value={contextValue}>
