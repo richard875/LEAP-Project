99263c1c55615e428efcf4910381999883922415
Richard Lee <hello@richard-lee.com>
2025-05-04 15:50:48 +1000

    - Feature: Add ele to Answer component
    Add github-markdown-css markdown styling

--- src/components/chat/Answer.tsx
+++ src/components/chat/Answer.tsx
@@ -1,17 +1,59 @@
+"use client";
+
+import Image from "next/image";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 import remarkGfm from "remark-gfm";
 import Markdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
-import "highlight.js/styles/github-dark-dimmed.css";
 import ConversationItem from "@/types/conversationItem";
 import ConversationType from "@/enums/conversationType";
+import "github-markdown-css";
+import "highlight.js/styles/github-dark-dimmed.css";
 
 const Answer = (answer: ConversationItem) => {
+  const { systemTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return (
     answer.type === ConversationType.Answer && (
-      <div className="react-markdown mt-7">
-        <Markdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeHighlight]}>
-          {answer.content}
-        </Markdown>
+      <div className="mt-7">
+        <div className="w-fit h-fit pl-2.5 pr-2.5 pt-2 pb-2 rounded-full border border-stone-300 dark:border-stone-500 flex items-center justify-center gap-2.5 select-none">
+          {systemTheme === "light" ? (
+            <Image
+              className="select-none"
+              src="/logo-small-light.png"
+              alt="LawConnect Logo"
+              width={20}
+              height={20}
+              priority
+            />
+          ) : (
+            <Image
+              className="select-none"
+              src="/logo-small-dark.png"
+              alt="LawConnect Logo"
+              width={20}
+              height={20}
+              priority
+            />
+          )}
+          <p className="text-sm font-medium">LawConnect</p>
+        </div>
+        <article className="react-markdown markdown-body !bg-transparent">
+          <Markdown
+            remarkPlugins={[remarkGfm]}
+            rehypePlugins={[rehypeHighlight]}
+          >
+            {answer.content}
+          </Markdown>
+        </article>
       </div>
     )
   );
