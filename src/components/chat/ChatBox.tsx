5dbc832d4d34ed2fbdb9a77c20d2f89ea7aeb00a
Richard Lee <hello@richard-lee.com>
2025-05-05 02:48:56 +1000

    - Feature: Apply Toast component

--- src/components/chat/ChatBox.tsx
+++ src/components/chat/ChatBox.tsx
@@ -6,8 +6,10 @@ import Image from "next/image";
 import { useTheme } from "next-themes";
 import { useContext } from "react";
 import { useEffect, useRef, useState } from "react";
+import { toast } from "sonner";
 import { v4 as uuidv4 } from "uuid";
 
+import { Toaster } from "@/components/ui/sonner";
 import ConversationContext from "@/context/conversationContext";
 import ApiRoute from "@/enums/apiRoute";
 import ConversationType from "@/enums/conversationType";
@@ -63,6 +65,13 @@ const ChatBox = () => {
     });
 
     if (!response.ok) {
+      toast(
+        `⚠️  Error: ${
+          response.statusText
+            ? response.statusText
+            : "Something went wrong. Please try again"
+        }`
+      );
       return;
     }
 
@@ -79,55 +88,58 @@ const ChatBox = () => {
   };
 
   return (
-    <footer className="w-full pl-5 pr-5">
-      <div className="max-w-[800px] m-auto flex flex-col items-center justify-around">
-        <div className="w-full min-h-14 pl-4 pr-4 bg-neutral-50 dark:bg-neutral-700 border border-stone-300 dark:border-stone-500 rounded-4xl shadow-xs flex items-center justify-between">
-          <div className="w-full flex items-center pt-2 pb-2">
-            {systemTheme === "light" ? (
-              <Image
-                className="select-none"
-                src="/logo-small-light.png"
-                alt="LawConnect Logo"
-                width={24}
-                height={24}
-                priority
+    <>
+      <footer className="w-full pl-5 pr-5">
+        <div className="max-w-[800px] m-auto flex flex-col items-center justify-around">
+          <div className="w-full min-h-14 pl-4 pr-4 bg-neutral-50 dark:bg-neutral-700 border border-stone-300 dark:border-stone-500 rounded-4xl shadow-xs flex items-center justify-between">
+            <div className="w-full flex items-center pt-2 pb-2">
+              {systemTheme === "light" ? (
+                <Image
+                  className="select-none"
+                  src="/logo-small-light.png"
+                  alt="LawConnect Logo"
+                  width={24}
+                  height={24}
+                  priority
+                />
+              ) : (
+                <Image
+                  className="select-none"
+                  src="/logo-small-dark.png"
+                  alt="LawConnect Logo"
+                  width={24}
+                  height={24}
+                  priority
+                />
+              )}
+              <textarea
+                ref={textareaRef}
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                onKeyDown={handleKeyDown}
+                placeholder="Ask a question..."
+                rows={1}
+                className="w-full max-h-[250px] resize-none focus:outline-none pl-3 pr-3"
               />
-            ) : (
-              <Image
-                className="select-none"
-                src="/logo-small-dark.png"
-                alt="LawConnect Logo"
-                width={24}
-                height={24}
-                priority
-              />
-            )}
-            <textarea
-              ref={textareaRef}
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              onKeyDown={handleKeyDown}
-              placeholder="Ask a question..."
-              rows={1}
-              className="w-full max-h-[250px] resize-none focus:outline-none pl-3 pr-3"
+            </div>
+            <FontAwesomeIcon
+              icon={faCircleArrowRight}
+              onClick={handleSubmit}
+              className={`text-2xl text-primary dark:text-white transition ${
+                disabled || context?.loading
+                  ? "brightness-65"
+                  : "hover:brightness-80 cursor-pointer"
+              }`}
             />
           </div>
-          <FontAwesomeIcon
-            icon={faCircleArrowRight}
-            onClick={handleSubmit}
-            className={`text-2xl text-primary dark:text-white transition ${
-              disabled || context?.loading
-                ? "brightness-65"
-                : "hover:brightness-80 cursor-pointer"
-            }`}
-          />
+          <p className="select-none text-center text-xs text-neutral-700 dark:text-neutral-300 pt-2 pb-2 pl-10 pr-10">
+            Our service provides general legal information, not legal advice.
+            Consult a lawyer for personalized assistance.
+          </p>
         </div>
-        <p className="select-none text-center text-xs text-neutral-700 dark:text-neutral-300 pt-2 pb-2 pl-10 pr-10">
-          Our service provides general legal information, not legal advice.
-          Consult a lawyer for personalized assistance.
-        </p>
-      </div>
-    </footer>
+      </footer>
+      <Toaster theme="system" />
+    </>
   );
 };
 
