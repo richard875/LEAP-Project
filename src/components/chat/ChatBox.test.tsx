797187d0fe4c2fbe1729c442680645c3e3ad27c4
Richard Lee <hello@richard-lee.com>
2025-05-05 06:07:00 +1000

    - Test: Add tests and snapshot for ChatBox

---
+++ src/components/chat/ChatBox.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from "@testing-library/react";
+
+import "@testing-library/jest-dom";
+
+import ChatBox from "./ChatBox";
+
+describe("ChatBox", () => {
+  it("should render correctly", () => {
+    const { container } = render(<ChatBox />);
+    expect(container).toBeTruthy();
+  });
+
+  it("should match the snapshot", () => {
+    const { asFragment } = render(<ChatBox />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("should have the correct elements in the component", () => {
+    render(<ChatBox />);
+
+    const textarea = screen.getByRole("textbox");
+    const logoLight = screen.getByAltText("LawConnect Logo");
+    const disclaimerText = screen.getByText(
+      "Our service provides general legal information, not legal advice. Consult a lawyer for personalized assistance."
+    );
+
+    expect(textarea).toBeInTheDocument();
+    expect(logoLight).toBeInTheDocument();
+    expect(disclaimerText).toBeInTheDocument();
+  });
+});
