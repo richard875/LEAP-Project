d8541c3a1a3ebffffb74b6d48b39071833a9f47c
Richard Lee <hello@richard-lee.com>
2025-05-05 04:52:33 +1000

    - Feature: Add starter text

--- src/components/page/Conversation.tsx
+++ src/components/page/Conversation.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { faWandMagicSparkles } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { motion, AnimatePresence } from "framer-motion";
 import { useContext } from "react";
 
@@ -21,6 +23,12 @@ const Conversation = () => {
 
   return (
     <main className="w-full flex-1 overflow-auto pl-5 pr-5">
+      {context?.conversation.length === 0 && (
+        <div className="w-full h-full flex items-center justify-center gap-2 text-neutral-500 dark:text-neutral-400">
+          <p className="text-2xl font-bold">Start a conversation below</p>
+          <FontAwesomeIcon className="text-xl" icon={faWandMagicSparkles} />
+        </div>
+      )}
       <div className="max-w-[800px] m-auto">
         <AnimatePresence initial={false}>
           {context?.conversation.map((item, index) => (
