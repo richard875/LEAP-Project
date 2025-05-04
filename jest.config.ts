1acb61e8eeb8939f835f63d73334d5949ec04234
Richard Lee <hello@richard-lee.com>
2025-05-05 05:55:57 +1000

    - Test: Modify Jest setup

--- jest.config.ts
+++ jest.config.ts
@@ -1,19 +1,20 @@
-import nextJest from "next/jest.js";
+import nextJest from "next/jest";
 
 import type { Config } from "jest";
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: "./",
 });
 
-// Add any custom config to be passed to Jest
 const config: Config = {
-  coverageProvider: "v8",
   testEnvironment: "jsdom",
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  coverageProvider: "v8",
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^react-markdown$": "<rootDir>/__mocks__/react-markdown.ts",
+  },
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default createJestConfig(config);
---
+++ jest.setup.ts
@@ -0,0 +1,13 @@
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  }),
+});
